ce94ab92c8908a6f45110f2bbb9041df9469091b
tabesin <tabesin@gmail.com>
2018-09-24 10:18:59 +0200

    Create debuild.sh

---
+++ debuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# debian build the two packages
+sudo apt-get -qq install libfile-fcntllock-perl  #installing files required to build plymouth
+cd meilix-artwork                                #cd into the metapackage directory
+echo y | debuild -uc -us #debuild the plymouth
