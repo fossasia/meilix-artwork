bf4754776dc6d714902b73d99e8d4dfcfbb0ccff
Abishek V Ashok <abishekvashok@gmail.com>
2018-10-10 20:19:38 +0530

    Cleans up the deb branch
    Cleans up the deb branch, to make it an empty branch. This would be used
    to contain the deb files built by Travis CI in the future.
    
    References: https://github.com/fossasia/meilix/issues/306/
    References: https://github.com/fossasia/meilix-artwork/issues/8
    
    Signed-off-by: Abishek V Ashok <abishekvashok@gmail.com>

--- debuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# debian build the two packages
-sudo apt-get -qq install libfile-fcntllock-perl  #installing files required to build plymouth
-cd meilix-artwork                                #cd into the metapackage directory
-echo y | debuild -uc -us #debuild the plymouth
