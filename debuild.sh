365f065300b2241e31b45935275f5ad6e40b3110
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-12-26 19:16:19 +0530

    Added necessary documentation

--- debuild.sh
+++ debuild.sh
@@ -2,6 +2,7 @@
 # debian build the two packages
 set -e
 sudo apt-get -qq install libfile-fcntllock-perl  #installing files required to build plymouth
+# Removing the previous text and logo packages
 sudo rm plymouth-theme-meilix-logo_*_all.deb
 sudo rm plymouth-theme-meilix-logo-2_*_all.deb
 sudo rm plymouth-theme-meilix-text_*_all.deb
