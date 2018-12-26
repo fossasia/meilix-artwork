c3cc6e36ae022985fc55e423a0d0d8cd74fcba6d
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-12-26 18:20:36 +0530

    Added versioning for the artwork package

--- debuild.sh
+++ debuild.sh
@@ -2,5 +2,8 @@
 # debian build the two packages
 set -e
 sudo apt-get -qq install libfile-fcntllock-perl  #installing files required to build plymouth
+sudo rm plymouth-theme-meilix-logo_*_all.deb
+sudo rm plymouth-theme-meilix-logo-2_*_all.deb
+sudo rm plymouth-theme-meilix-text_*_all.deb
 cd meilix-artwork                                #cd into the metapackage directory
 echo y | debuild -uc -us #debuild the plymouth
