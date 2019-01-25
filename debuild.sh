d224584f01db3459a4ae028845411bd479d20f56
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2019-01-25 20:55:11 +0530

    Added deployment for the default theme package (#96)

--- debuild.sh
+++ debuild.sh
@@ -7,5 +7,6 @@ sudo rm plymouth-theme-meilix-logo_*_all.deb
 sudo rm plymouth-theme-meilix-logo-2_*_all.deb
 sudo rm plymouth-theme-meilix-text_*_all.deb
 sudo rm sddm-theme-meilix_*_all.deb
+sudo rm meilix-default-theme_*_all.deb
 cd meilix-artwork                                #cd into the metapackage directory
 echo y | debuild -uc -us #debuild the plymouth
